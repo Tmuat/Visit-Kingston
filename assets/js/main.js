03a5ff36253c77468c4c370f833991f1c3236106
Tmuat <Thomas_Muat@hotmail.com>
2020-11-17 15:47:32 +0000

    Added more comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 // Script to start the AOS library from https://michalsnik.github.io/aos/
 
+// Starts the animation on scroll library 
 AOS.init({
-        duration: 1000,
-    });
+    // Sets the duration of the animations
+    duration: 1000,
+});
