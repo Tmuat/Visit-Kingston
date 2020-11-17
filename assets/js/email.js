f06041ad3dbe6fca343da53bd87d90f0f6489803
Tmuat <Thomas_Muat@hotmail.com>
2020-11-17 15:46:25 +0000

    Added comments to email.js doc

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,8 +1,12 @@
 // EmailJS taken from https://www.emailjs.com/docs/sdk/installation/ & https://codeinstitute.net/ 
 
+// Set the function to be called upon form submission on the contact page
 function sendMail(contactForm) {
+    // Changes the submit button to a loading gif so users know the form is submitting
     $("#form-submit-button").html(`<img class="loader" src="assets/media/images/loading_bars.gif"/>`);
+    // EmailJS user ID
     emailjs.init("user_dXKiXtry6J70AYkRbQh46");
+    // EmailJS function to send email, taking information from the contact form to fill template
     emailjs.send('service_sta4808', 'ms2_template', {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
@@ -11,11 +15,14 @@ function sendMail(contactForm) {
     })
     .then(
         function(response) {
+            // On successfull submission, clears the form
             $("#form-name").val("");
             $("#form-email").val("");
             $("#form-subject").val("");
             $("#form-text").val("");
+            // Sets the submit button back to submit as opposed to loading gif
             $("#form-submit-button").html("Submit");
+            // Puts up an alert to acknowledge form submission
             // Code for the alert from https://getbootstrap.com/docs/4.0/components/alerts/#dismissing
             $("#email-alert").html(`
                 <div class="alert alert-success alert-dismissible fade show mb-0" role="alert">
@@ -27,7 +34,9 @@ function sendMail(contactForm) {
             `);
         },
         function(error) {
+            // Changes the button from loading gif to re-submit
             $("#form-submit-button").html("Re-Submit");
+            // Puts up an alert to highlight error in form submission
             // Code for the alert from https://getbootstrap.com/docs/4.0/components/alerts/#dismissing
             $("#email-alert").html(`
                 <div class="alert alert-danger alert-dismissible fade show mb-0" role="alert">
@@ -39,5 +48,6 @@ function sendMail(contactForm) {
             `);
         }
     );
+    // Stops the whole page from reloading on submission
     return false;
 }
